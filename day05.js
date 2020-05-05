419536be2c169162d460c2f3e6d4340e59a74771
stanley-c-so <stanley.c.so@gmail.com>
2020-05-05 11:00:23 -0400

    updated day05

--- day05.js
+++ day05.js
@@ -14,6 +14,7 @@
 
 // ----------
 
+// just make a frequency object and return the index of the first letter that has frequency 1 (or -1 if none)
 function solution_1 (s) {
   const freq = {};
   for (const char of s) {
@@ -25,9 +26,17 @@ function solution_1 (s) {
   return -1;
 }
 
+// one-liner - basically the above
 var solution_2=(s,f={},S=[...s],r=-1)=>(S.map(c=>f[c]=(f[c]||0)+1),S.map((c,i)=>r=f[c]==1&&r<0?i:r))|r
 
-const firstUniqChar = solution_2;
+// thomas luo's improvement on my one-liner - using `o[e]+1|1` instead of `(f[c]||0)+1`, ... && ... to avoid (... , ...), and chained ternary
+// statements as a substitute for checking `f[c]==1&&r<0` (although this is not better!!)
+var solution_3=(s,p=[...s],o={},z=-1)=>p.map(e=>o[e]=o[e]+1|1)&&p.map((e,i)=>o[e]-1?0:z+1?z:z=i)&&z
+
+// hybrid of our one-liners - borrowed a few of thomas' ideas, and refactored the ternary to shorten by 1 character. also brought back `|r`
+var solution_4=(s,S=[...s],f={},r=-1)=>S.map(c=>f[c]=f[c]+1|1)&&S.map((c,i)=>f[c]-1||r+1?0:r=i)|r
+
+const firstUniqChar = solution_4;
 
 // const specialTest = (...args) => {
 // };
